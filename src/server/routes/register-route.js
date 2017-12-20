d394de496a2964ea9792ce8309c4c4a2420c4f51
hdthuc93 <hdthuc93@gmail.com>
2017-12-20 20:05:29 +0700

    create new branch

--- src/server/routes/register-route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import registerCtrl from '../controllers/register-controller';
-const router = express.Router();
-
-router.route('/')
-  .post((req, res) => registerCtrl.Register(req, res));
-
-export default router;
