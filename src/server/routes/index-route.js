db8b9a4d237aebaac4174c786f3a1b3cc774d85c
hdthuc93 <hdthuc93@gmail.com>
2017-12-28 13:38:01 +0700

    add user auth

--- src/server/routes/index-route.js
+++ src/server/routes/index-route.js
@@ -2,11 +2,15 @@ import express from 'express';
 import userRoute from './user-route';
 import problemRoute from './problem-route';
 import solutionRoute from './solution-route';
+import auth from '../middlewares/authentication';
 
 
 const router = express.Router();
 
 router.use('/users', userRoute);
+
+router.use('/*', auth.authenToken);
+
 router.use('/problems', problemRoute);
 router.use('/solutions', solutionRoute);
 
